73e5883f2429f3adeeb9668a8cbdecfffd213685
Italo Oliveira <italooliveiraxdd@gmail.com>
2025-11-13 10:15:46 -0300

    test: corrige erro no test soma

--- math_test.go
+++ math_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestSoma(t *testing.T) {
 
 	total := Soma(15, 15)
-	valorEsperado := 10
+	valorEsperado := 30
 
 	if total != valorEsperado {
 		t.Errorf("Resultado da soma é inválido! Valor esperado é %d e o resultado foi %d.", valorEsperado, total)
